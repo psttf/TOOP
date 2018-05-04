d309d4b657741852ae1aeba904eb7a7b5520ae76
IIpocTo <allxf95@gmail.com>
2018-05-04 03:23:52 +0300

    #5 | Complete issue, add scalafmt config.

--- build.sbt
+++ build.sbt
@@ -5,7 +5,8 @@ val scalaVersionValue = "2.12.6"
 val defaultScalacOptions = Seq("-deprecation", "-encoding", "utf-8")
 
 val parserCombinators = "org.scala-lang.modules" %% "scala-parser-combinators" % "1.1.0"
-val specs2_core = "org.specs2" %% "specs2-core" % "4.1.0"
+val specs2_core = "org.specs2" %% "specs2-core" % "4.2.0"
+val monix = "io.monix" %% "monix" % "2.3.3"
 val webjars_play = "org.webjars" %% "webjars-play" % "2.6.3"
 val bootstrap = "org.webjars" % "bootstrap" % "4.1.0"
 val ace_builds = "org.webjars.bower" % "ace-builds" % "1.3.3"
@@ -26,10 +27,11 @@ lazy val toopCore = (project in file("toop-core"))
 lazy val toopWeb = (project in file("toop-web"))
   .settings(
     name := "toop-web",
-    version := "1.0",
+    version := "1.1",
     scalaVersion := scalaVersionValue,
     libraryDependencies ++= Seq(
       webjars_play,
+      monix,
       bootstrap,
       ace_builds,
       jquery,
--- toop-web/app/controllers/Application.scala
+++ toop-web/app/controllers/Application.scala
@@ -1,31 +1,63 @@
 package controllers
 
+import java.util.concurrent.Executors
+
 import javax.inject.{Inject, Singleton}
 import expressions.{Parser, Semantic}
+import monix.execution.FutureUtils.extensions._
+import monix.execution.Scheduler
+import play.api.Configuration
 import play.api.data.Form
 import play.api.data.Forms._
-import play.api.mvc.{AbstractController, ControllerComponents}
+import play.api.mvc.{
+  AbstractController,
+  Action,
+  AnyContent,
+  ControllerComponents
+}
+
+import scala.concurrent._
+import scala.concurrent.duration._
+import scala.util.Failure
 
 @Singleton
-class Application @Inject()(cc: ControllerComponents, indexTemplate: views.html.index)
-                           (implicit assetsFinder: AssetsFinder)
-  extends AbstractController(cc) {
+class Application @Inject()(
+  cc: ControllerComponents,
+  config: Configuration,
+  indexTemplate: views.html.index
+)(implicit assetsFinder: AssetsFinder)
+    extends AbstractController(cc) {
+
+  val ec: ExecutionContextExecutor =
+    ExecutionContext.fromExecutor(Executors.newFixedThreadPool(10))
+
+  implicit val scheduler: Scheduler = Scheduler(ec)
 
   val evalForm = Form(
     "code" -> text
   )
 
   def index = Action {
-    Ok(indexTemplate(evalForm fill views.txt.code().toString, None))
+    Ok(indexTemplate(evalForm.fill(views.txt.code().toString), None))
   }
 
-  def eval = Action { implicit request =>
+  def eval: Action[AnyContent] = Action.async { implicit request =>
     val form = evalForm.bindFromRequest
-    form("code").value.map({ code =>
-      val result = Parser parse code map Semantic.eval
-      println(s"result = $result")
-      Ok(indexTemplate(form, Some(result)))
-    }) getOrElse PreconditionFailed(indexTemplate(form, None))
+    form("code").value
+      .map({ code =>
+        val result = Future { Parser.parse(code).map(Semantic.eval) }
+          .timeoutTo(
+            config.get[Int]("parserFuture.timeoutInSeconds").seconds,
+            Future.failed(new TimeoutException)
+          )
+        result
+          .map(parsedTerm => Ok(indexTemplate(form, Some(parsedTerm))))
+          .recoverWith {
+            case err: TimeoutException =>
+              Future { Ok(indexTemplate(form, Some(Failure(err)))) }
+          }
+      })
+      .getOrElse(Future { PreconditionFailed(indexTemplate(form, None)) })
   }
 
 }
