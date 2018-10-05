8305a38b50c63b592ab490090e02ebb640455ad4
eakarpov <eakarpov@yandex.ru>
2018-10-05 23:31:32 +0300

    fixed resource paths

---
+++ toop-cli/windows/sigmac/src/tools/CHANGELOG.md
@@ -0,0 +1,5 @@
+##0.1.1 (October 05, 2018)
+
+BUG FIXES:
+
+* Fix - Resource path to sigmac.jar and sigmac.bat while installing package
--- toop-cli/windows/sigmac/src/tools/chocolateyInstall.ps1
+++ toop-cli/windows/sigmac/src/tools/chocolateyInstall.ps1
@@ -6,7 +6,7 @@ try {
 }
 New-Item -ItemType directory -Path "C:\Program Files\sigmac"
 New-Item -ItemType directory -Path "C:\Program Files\sigmac\bin"
-Copy-Item -Path ./sigmac.jar  -Force -Destination "C:\Program Files\sigmac\sigmac.jar"
-Copy-Item -Path ./sigmac.bat -Force -Destination "C:\Program Files\sigmac\bin\sigmac.bat"
+Copy-Item -Path "C:\ProgramData\chocolatey\lib\sigmac\tools\sigmac.jar"  -Force -Destination "C:\Program Files\sigmac\sigmac.jar"
+Copy-Item -Path "C:\ProgramData\chocolatey\lib\sigmac\tools\sigmac.bat" -Force -Destination "C:\Program Files\sigmac\bin\sigmac.bat"
 setx PATH "$env:path;C:\Program Files\sigmac\bin" -m
 
